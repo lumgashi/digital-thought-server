18373d2f4c602c45e72a41e63d1001e6485bb599
lumgashi <lumgashi97@hotmail.com>
2023-11-18 18:13:05 +0100

    added endpoint to get user by id

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HttpStatus } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -14,5 +17,27 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Should return a user object with valid id
+  it('should return a user object with valid id', async () => {
+    const usersService = new UsersService(new PrismaService());
+    const id = 'validId';
+    const user = {
+      id: 'validId',
+      firstName: 'John Doe',
+      email: 'johndoe@example.com',
+    };
+    jest
+      .spyOn(usersService['prisma'].user, 'findUnique')
+      .mockResolvedValue(user);
+
+    const result = await usersService.findOne(id);
+
+    expect(result).toEqual({
+      success: true,
+      status: HttpStatus.OK,
+      data: user,
+    });
   });
 });
