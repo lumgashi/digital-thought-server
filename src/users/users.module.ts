30021ed146b9afb1c287e7bf1d9265b87b26a72b
lumgashi <lumgashi97@hotmail.com>
2023-11-18 19:42:58 +0100

    added endpoint to get all users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PaginationService } from 'src/pagination/pagination.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PaginationService],
   exports: [UsersService],
 })
 export class UsersModule {}
